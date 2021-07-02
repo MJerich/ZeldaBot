2cfbd626e51dfb0e8498f7b4026e645dd3f82650
MJerich <MatthewJerich@gmail.com>
2021-07-01 21:18:07 -0700

    Update musicBotCog.py
    bot commands for music now only work in the 'music-bot-commands' channel. if typed in the wrong channel it will direct you where to go.

--- cog/musicBotCog.py
+++ cog/musicBotCog.py
@@ -9,6 +9,9 @@ def is_connected(ctx):
     voice_client = discord.utils.get(ctx.bot.voice_clients)
     return voice_client and voice_client.is_connected()
 
+botCC = 801828129068023809
+wrngChannel = f'Please use music bot commands in <#{botCC}>'
+
 class musicCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -16,66 +19,87 @@ class musicCommands(commands.Cog):
     # play command, must be followed with a youtube url
     @commands.command(name='play')
     async def play(self, ctx, url: str):
-        songPlaying = os.path.isfile('song.mp3')
-        try:
-            if songPlaying:
-                os.remove('song.mp3')
-        except PermissionError:
-            await ctx.send('wait for the current song to finish please.')
-            return
+        if ctx.message.channel.id == botCC:
+            songPlaying = os.path.isfile('song.mp3')
+            try:
+                if songPlaying:
+                    os.remove('song.mp3')
+            except PermissionError:
+                await ctx.send('wait for the current song to finish please.')
+                return
 
-        voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='Reign\'s music studio')
+            voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='Reign\'s music studio')
 
-        if not is_connected(ctx):
-            await voiceChannel.connect()
+            if not is_connected(ctx):
+                await voiceChannel.connect()
 
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }]
-        }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-        for file in os.listdir('./'):
-            if file.endswith('.mp3'):
-                os.rename(file, 'song.mp3')
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }]
+            }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+            for file in os.listdir('./'):
+                if file.endswith('.mp3'):
+                    os.rename(file, 'song.mp3')
 
-        ctx.voice_client.play(discord.FFmpegPCMAudio('song.mp3'))
+            ctx.voice_client.play(discord.FFmpegPCMAudio('song.mp3'))
+        else:
+            #command attempted in non command channel - redirect user
+            await ctx.message.channel.send(wrngChannel)
 
     # make bot leabve the voice channel
     @commands.command(name='leave')
     async def leave(self, ctx):
-        if ctx.voice_client.is_connected():
-            await ctx.voice_client.disconnect()
+        if ctx.message.channel.id == botCC:
+            if ctx.voice_client.is_connected():
+                await ctx.voice_client.disconnect()
+            else:
+                await ctx.send('The bot is not in a voice channel.')
         else:
-            await ctx.send('The bot is not in a voice channel.')
+            #command attempted in non command channel - redirect user
+            await ctx.message.channel.send(wrngChannel)
 
     # pause the music
     @commands.command(name='pause')
     async def pause(self, ctx):
-        if ctx.voice_client.is_playing():
-            ctx.voice_client.pause()
+        if ctx.message.channel.id == botCC:
+            if ctx.voice_client.is_playing():
+                ctx.voice_client.pause()
+            else:
+                await ctx.send('Nothing is playing.')
         else:
-            await ctx.send('Nothing is playing.')
+            #command attempted in non command channel - redirect user
+            await ctx.message.channel.send(wrngChannel)
 
     #resume the music after pausing
     @commands.command(name='resume')
     async def resume(self, ctx):
-        if ctx.voice_client.is_paused():
-            ctx.voice_client.resume()
+        if ctx.message.channel.id == botCC:
+            if ctx.voice_client.is_paused():
+                ctx.voice_client.resume()
+            else:
+                await ctx.send('Audio is already playing.')
         else:
-            await ctx.send('Audio is already playing.')
+            #command attempted in non command channel - redirect user
+            await ctx.message.channel.send(wrngChannel)
 
     # stop the music
     @commands.command(name='stop')
     async def stop(self, ctx):
-        if ctx.voice_client.is_playing():
-            ctx.voice_client.stop()
+        if ctx.message.channel.id == botCC:
+            if ctx.voice_client.is_playing():
+                ctx.voice_client.stop()
+            else:
+                await ctx.send('Nothing is playing.')
         else:
-            await ctx.send('Nothing is playing.')
+            #command attempted in non command channel - redirect user
+            await ctx.message.channel.send(wrngChannel)
+
 
 
 def setup(bot):
